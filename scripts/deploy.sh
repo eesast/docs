3fcf38cf8e2c9f95a0fc4ec037d6537841315040
Campbell He <duskmoon314@users.noreply.github.com>
2020-11-14 03:40:17 +0800

    Upload file to server before deploy to gh-pages (#6)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
+yarn && yarn build
+scp -o StrictHostKeyChecking=no -r build/* "${SERVER_USR}@${SERVER_IP}":"${DOC_DIR}"
 git config --global user.name "${GH_NAME}"
 git config --global user.email "${GH_EMAIL}"
 echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
-yarn && GIT_USER="${GH_NAME}" yarn deploy
-openssl aes-256-cbc -K $encrypted_2d8c63ef40a5_key -iv $encrypted_2d8c63ef40a5_iv -in id_rsa.enc -out id_rsa -d
-scp -i id_rsa -r build/* "${SERVER_IP}":"${DOC_DIR}"
+GIT_USER="${GH_NAME}" yarn deploy
